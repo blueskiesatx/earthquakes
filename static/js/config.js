17cf4581d12c07f290a465fc2adf2bf848fbe9b4
Mary Salazar <msalazar.chembio@gmail.com>
2022-06-15 17:24:04 -0500

    config2
    mapbox tweek2

--- static/js/config.js
+++ static/js/config.js
@@ -2,4 +2,4 @@
 //this one is public default key
 //const API_KEY = "pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzZTV3YTFlYTk0OW1ybWVkNXA0cGUifQ.3MKJBvS6N9fXLwvqKpEAew";
 //mykey
-const API_KEY = "pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzaGphbDFlNTEzenN6ampkejRkNWIifQ.Xi_VpSSkjV45tmqw_pMH8w";
+const API_KEY = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA#1.07/0/0";

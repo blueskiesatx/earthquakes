3fa04fc02aede9e07fcedb468a482be03e10e0af
Mary Salazar <msalazar.chembio@gmail.com>
2022-07-22 12:10:22 -0500

    tile street v8

--- logic.js
+++ logic.js
@@ -80,10 +80,16 @@ function createMap() {
         accessToken: 'pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzZTV3YTFlYTk0OW1ybWVkNXA0cGUifQ.3MKJBvS6N9fXLwvqKpEAew'
     });
 
-    var streetMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//    var streetMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//        maxZoom: 18,
+//        id: 'mapbox.streets',
+//        accessToken: 'pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzZTV3YTFlYTk0OW1ybWVkNXA0cGUifQ.3MKJBvS6N9fXLwvqKpEAew'
+//    });
+       var streetMap = L.tileLayer('https://api.tiles.mapbox.com/v8/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox.streets',
+        id: 'mapbox.mapbox-streets-v8',
         accessToken: 'pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzZTV3YTFlYTk0OW1ybWVkNXA0cGUifQ.3MKJBvS6N9fXLwvqKpEAew'
     });
 

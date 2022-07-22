ca79924e9b37e930b37d92ad2835a1b3d3909ca4
Mary Salazar <msalazar.chembio@gmail.com>
2022-07-22 12:00:52 -0500

    API change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // API key
 // const API_KEY = "pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzaGphbDFlNTEzenN6ampkejRkNWIifQ.Xi_VpSSkjV45tmqw_pMH8w";
-const API_KEY = "pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzZTV3YTFlYTk0OW1ybWVkNXA0cGUifQ.3MKJBvS6N9fXLwvqKpEAew";
+const API_KEY = "pk.eyJ1IjoidHJpcHBpbjIxIiwiYSI6ImNqc3IzaGphbDFlNTEzenN6ampkejRkNWIifQ.Xi_VpSSkjV45tmqw_pMH8w";
